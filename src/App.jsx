fccb1d03f56205d48f33dd838aeb89435ab41aef
Ankit Lingwal <ankitlingwal090@gmail.com>
2024-11-01 12:40:02 +0530

    Stage-0

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { TokenLaunchpad } from './components/TokenLaunchpad'
+
+
+function App() {
+  return (
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            padding: 10
+          }}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+          <TokenLaunchpad />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+export default App
